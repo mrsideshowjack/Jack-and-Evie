801b870c2ec98d98955ad1eb81328035cd182319
Jack Mason <jack.mason1@ibm.com>
2021-02-09 10:22:59 +0000

    misc: temp remove plugin.

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,48 +7,48 @@
 module.exports = {
   siteName: 'Jack & Evie',
   plugins:[
-    {
-        use: 'gridsome-plugin-pwa',
-        options: {
-            // Service Worker Options
-            disableServiceWorker: false,
-            serviceWorkerPath: 'service-worker.js',
-            cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg,gif',
-            disableTemplatedUrls: false,       // Optional
+    // {
+    //     use: 'gridsome-plugin-pwa',
+    //     options: {
+    //         // Service Worker Options
+    //         disableServiceWorker: false,
+    //         serviceWorkerPath: 'service-worker.js',
+    //         cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg,gif',
+    //         disableTemplatedUrls: false,       // Optional
 
-            // Manifest Options (https://developer.mozilla.org/en-US/docs/Web/Manifest)
-            manifestPath: 'manifest.json',
-            title: 'Jack & Evie',
-            startUrl: '/',
-            display: 'standalone',
-            statusBarStyle: 'default',
-            themeColor: '#666600',
-            backgroundColor: '#ffffff',
-            icon: '',
-            shortName: 'Jack & Evie',              // Optional
-            description: 'Jack & Evie wedding',// Optional
-            lang: 'en-GB',                      // Optional
-            dir: 'auto',                        // Optional
-            maskableIcon: true,                 // Optional
-            screenshots: [                      // Optional
-                {
-                    src: 'src/screenshot1.png',
-                    sizes: '1280x720',
-                    type: 'image/png',
-                },
-            ],
-            // gcmSenderId: undefined,             // Optional
+    //         // Manifest Options (https://developer.mozilla.org/en-US/docs/Web/Manifest)
+    //         manifestPath: 'manifest.json',
+    //         title: 'Jack & Evie',
+    //         startUrl: '/',
+    //         display: 'standalone',
+    //         statusBarStyle: 'default',
+    //         themeColor: '#666600',
+    //         backgroundColor: '#ffffff',
+    //         icon: '',
+    //         shortName: 'Jack & Evie',              // Optional
+    //         description: 'Jack & Evie wedding',// Optional
+    //         lang: 'en-GB',                      // Optional
+    //         // dir: 'auto',                        // Optional
+    //         // maskableIcon: true,                 // Optional
+    //         // screenshots: [                      // Optional
+    //         //     {
+    //         //         src: 'src/screenshot1.png',
+    //         //         sizes: '1280x720',
+    //         //         type: 'image/png',
+    //         //     },
+    //         // ],
+    //         // gcmSenderId: undefined,             // Optional
 
-            // Standard Meta Tags
-            // svgFavicon: 'favicon.svg',          // Optional. Requires favicon.ico fallback
+    //         // Standard Meta Tags
+    //         //svgFavicon: 'favicon.svg',          // Optional. Requires favicon.ico fallback
 
-            // Microsoft Windows Meta Tags
-            msTileColor: '#666600',             // Optional
+    //         // Microsoft Windows Meta Tags
+    //         msTileColor: '#666600',             // Optional
 
-            // Apple MacOS Meta Tags
-            appleMaskIcon: 'favicon.svg',       // Optional
-            appleMaskIconColor: '#666600',      // Optional
-        }
-    }
+    //         // Apple MacOS Meta Tags
+    //         // appleMaskIcon: 'favicon.svg',       // Optional
+    //         // appl eMaskIconColor: '#666600',      // Optional
+    //     }
+    // }
   ]
 }
